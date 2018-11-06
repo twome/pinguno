13b51f8b5cd0c3abe24d5c1aa760f247014fb6ec
Tom Kenny <tom@twome.name>
2018-11-06 14:56:11 +0800

    Make custom live-reload try and restart if the user comes back to the page

--- browser/js/live-reload-custom.js
+++ browser/js/live-reload-custom.js
@@ -1,4 +1,5 @@
 /* eslint no-console: 0 */
+import throttle from '../node_modules/lodash-es/throttle.js'
 
 let { DateTime } = require('../node_modules/luxon/build/cjs-browser/luxon.js')
 
@@ -9,11 +10,12 @@ let defaultLiveReloadPath = '/dev/client-code-last-modified'
 // State
 let clientLastRefreshedDate = new Date()
 let sequentialFailures = 0
-let defaultPort = Number(location.port) + 1
-let portToUse = defaultPort + sequentialFailures
+let nextPort = Number(location.port) + 1
+let portToUse = nextPort + sequentialFailures
 let fetchURL = new URL(defaultLiveReloadPath, location.protocol + '//' + location.hostname + ':' + portToUse)
 
 let liveReloadTick = ()=>{
+	if (sequentialFailures >= 5) return false
 	fetch(fetchURL.href).then((res)=>{
 		res.text().then(responseText => {
 			if (!responseText){	return false }
@@ -29,7 +31,7 @@ let liveReloadTick = ()=>{
 		sequentialFailures += 1
 		if (sequentialFailures >= 5){
 			clearInterval(liveReloadTick)
-			console.warn(`Live reload failed to connect${sequentialFailures} times in a row; assuming the server's down.`)
+			console.warn(`Live reload failed to connect ${sequentialFailures} times in a row; assuming the server's down.`)
 		} else {
 			console.warn(`Live reload failed to connect to ${fetchURL.href} - trying again at port ${portToUse}`)
 			// Try again at an incremented port
@@ -38,3 +40,13 @@ let liveReloadTick = ()=>{
 	})
 }
 setInterval(liveReloadTick, 1000)
+
+let restart = throttle(()=>{
+	sequentialFailures = 0
+	setInterval(liveReloadTick, 1000)
+}, 1000)
+
+// If we detect user activity, try and restart the live reloading connection attempts
+document.addEventListener('focusin keydown scroll', ()=>{
+	restart()
+})
